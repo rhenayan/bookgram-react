82c888d1c023b307a39c98004233fbd68013b4ba
Rhea Abayan <rhenayanz@gmail.com>
2022-08-30 11:31:16 +0800

    updated browser router

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './styles/scss/search/search.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     
         <FavoriteContextProvider>
           <SearchContextProvider>
